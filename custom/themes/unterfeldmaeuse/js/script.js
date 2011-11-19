1c2fc1ea9c3f5b2c8d8e879c6a237b4c99edc14f
Marc-Oliver Teschke <marcoliverteschke@mac.com>
2011-11-19 20:27:22 +0100

    Guestbook
    Styled the guestbook.
    Added script to hide the guestbook form on load.
    Added a module to redirect form guestbook nodes.
    
    Signed-off-by: Marc-Oliver Teschke <marcoliverteschke@mac.com>

--- custom/themes/unterfeldmaeuse/js/script.js
+++ custom/themes/unterfeldmaeuse/js/script.js
@@ -1,3 +1,11 @@
-$(function(){
-
-});
+(function ($) {
+	$(document).ready(function(){
+		if($('#block-formblock-gaestebucheintrag').length == 1)
+		{
+			$('#block-formblock-gaestebucheintrag h2').addClass('toggle').click(function(){
+				$('#block-formblock-gaestebucheintrag .content').slideToggle();
+			});
+			$('#block-formblock-gaestebucheintrag .content').hide();
+		}
+	});
+})(jQuery);
---
+++ custom/themes/unterfeldmaeuse/views-view-unformatted--gaestebuch--page.tpl.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @file views-view-unformatted.tpl.php
+ * Default simple view template to display a list of rows.
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php //module_load_include('inc', 'node', 'node.pages'); ?>
+	
+<?php //echo node_add(); ?>
+	
+<?php if (!empty($title)): ?>
+  <h3><?php print $title; ?></h3>
+<?php endif; ?>
+<?php foreach ($rows as $id => $row): ?>
+  <div class="<?php print $classes_array[$id]; ?>">
+    <?php print $row; ?>
+  </div>
+<?php endforeach; ?>
