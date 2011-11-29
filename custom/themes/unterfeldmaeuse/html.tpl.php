e23c03b296b4133674d6929de1bef45d49516e1f
Marc-Oliver Teschke <marcoliverteschke@mac.com>
2011-11-29 14:13:48 +0100

    Added meta-description to template. Added favicon.

--- custom/themes/unterfeldmaeuse/html.tpl.php
+++ custom/themes/unterfeldmaeuse/html.tpl.php
@@ -48,8 +48,7 @@
 <!--[if (gt IE 9)|!(IE)]><!--> <html lang="<?php print $language->language; ?>" class="no-js" <?php print $rdf_namespaces; ?>> <!--<![endif]-->
 <head>
 	<meta charset="UTF-8">
-	<meta name="description" content="">
-	<meta name="author" content="">
+	<meta name="description" content="Die Theatergruppe UNTERFELDMÄUSE aus Erkrath unterhält seit 1983 ihr Publikum mit Komödien und Kinderstücken. Hier finden Sie Informationen zum Verein, den Stücken, Kartenvorbestellung und mehr.">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<?php print $head; ?>
 
