59d05f35700c61e526ad0810b09e9d2686f887e8
Marc-Oliver Teschke <marcoliverteschke@mac.com>
2011-11-29 07:15:40 +0100

    Added Analytics tracking code.

--- custom/themes/unterfeldmaeuse/html.tpl.php
+++ custom/themes/unterfeldmaeuse/html.tpl.php
@@ -65,6 +65,19 @@
 		<link rel="stylesheet" href="/<?php echo path_to_theme() ?>/ie6.1.1.css" />
 	<![endif]-->
 	<?php print $scripts; ?>
+	<script type="text/javascript">
+
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-27337308-1']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+
+	</script>
 </head>
 
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
